9d6a20cb3f54f71bcf585cc5dee283d0613d9819
Adam Arnesen <adamarnesen@gmail.com>
2021-10-29 15:31:40 -0500

    soft methods and add __ for privates

--- wavegen/UniformWaveform.py
+++ wavegen/UniformWaveform.py
@@ -15,67 +15,67 @@ class generator:
         xMin,xMax = np.min(xBounds), np.max(xBounds)
         xPointCount = 1000
         x = np.linspace(xMin, xMax, num=xPointCount)
-        y = self.gen_random_uniform_based_waveform(x, segments)
+        y = self.__gen_random_uniform_based_waveform(x, segments)
         waveforms = []
         for n in tqdm(range(waveformsPerFamily), desc=f'Generating Waveforms for family {familyNumber}'):
             lineBounds = rnd.random(2)
             line = np.linspace(lineBounds.min(), lineBounds.max(), num=xPointCount)
             ys = y * line
             if n in errorIndexes:
-                ys = self.introduce_error(ys)
+                ys = self.__introduce_error(ys)
             waveforms.append(ys)
-        waveforms = self.add_noise(waveforms)
+        waveforms = self.__add_noise(waveforms)
         return x, waveforms, errorIndexes
     
-    def gen_random_uniform_based_waveform(self, x, segments):
+    def __gen_random_uniform_based_waveform(self, x, segments):
         y = (x - rnd.uniform(-1, 1))
         for s in range(segments):
             y = y * (x - rnd.uniform(-1, 1))
         return y
 
-    def introduce_error(self, y):
+    def __introduce_error(self, y):
         errorType = rnd.randint(0,3)
         if errorType == 0:
-            return self.introduce_narrow_spike(y)
+            return self.__introduce_narrow_spike(y)
         elif errorType == 1:
-            return self.introduce_step(y)
+            return self.__introduce_step(y)
         elif errorType ==2:
-            return self.inroduce_constant_error(y)
+            return self.__introduce_constant_error(y)
         return y
 
-    def get_error_range(self, y):
-        bounds = rnd.rand(2)
-        maxErrorLength = len(y) / 2
-        return math.floor(np.min(bounds) * maxErrorLength), math.ceil(np.max(bounds) * maxErrorLength)
-
-    def get_random_value_in_y_range_with_factor(self, y):
-        errorScale = rnd.uniform(-1,1)
-        percentRangeForError = 1.5
-        return (np.max(y) - np.min(y)) * errorScale * percentRangeForError
-        
-    def introduce_narrow_spike(self, y):
+    def __introduce_narrow_spike(self, y):
         y_error = y
         min, max = np.min(y), np.max(y)
         spike_height = np.abs(max-min) * rnd.random()
         errorPoint = rnd.randint(0,len(y))
         y_error[errorPoint] = y_error[errorPoint] + spike_height
         return y_error
-
-    def introduce_step(self, y):
+    
+    def __introduce_step(self, y):
         y_error = y
-        errorPointMin, errorPointMax = self.get_error_range(y)
-        delta = self.get_random_value_in_y_range_with_factor(y)
+        errorPointMin, errorPointMax = self.__get_error_range(y)
+        delta = self.__get_random_value_in_y_range_with_factor(y)
         y[errorPointMin:errorPointMax] = y[errorPointMin : errorPointMax] + delta
         return y_error
 
-    def inroduce_constant_error(self, y):
+    def __introduce_constant_error(self, y):
         y_error = y
-        min, max = self.get_error_range(y)
-        error_value = self.get_random_value_in_y_range_with_factor(y)
+        min, max = self.__get_error_range(y)
+        error_value = self.__get_random_value_in_y_range_with_factor(y)
         y[min:max] = error_value
         return y_error
 
-    def add_noise(self, waveforms):
+    def __get_error_range(self, y):
+        bounds = rnd.rand(2)
+        maxErrorLength = len(y) / 2
+        return math.floor(np.min(bounds) * maxErrorLength), math.ceil(np.max(bounds) * maxErrorLength)
+
+    def __get_random_value_in_y_range_with_factor(self, y):
+        errorScale = rnd.uniform(-1,1)
+        percentRangeForError = 1.5
+        return (np.max(y) - np.min(y)) * errorScale * percentRangeForError
+
+    def __add_noise(self, waveforms):
         scale = (np.max(waveforms) - np.min(waveforms)) * np.abs(rnd.normal(0,.005))
         withNoise = []
         for w in waveforms:
