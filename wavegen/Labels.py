37710e210d78c91ef749effb22e51d9f54216024
Adam Arnesen <adamarnesen@gmail.com>
2021-11-01 11:52:08 -0500

    format files

--- wavegen/Labels.py
+++ wavegen/Labels.py
@@ -3,6 +3,7 @@ import os
 
 LABEL_FILE = 'labels.csv'
 
+
 def save_labels_to_csv(savefolder, labels):
     with open(os.path.join(savefolder, LABEL_FILE), 'a', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
--- wavegen/RandomWaveform.py
+++ wavegen/RandomWaveform.py
@@ -3,19 +3,25 @@ import MatplotlibFormat as mplf
 
 IMAGE_DIRECTORY = 'images'
 
+
 def gen_random_square_wave():
     # make this have random heights (gausian in some range)
     return 1
+
 
 def gen_uniform_based_waveforms(numFamilies, waveformsPerFamily, genFamiliesOnSinglePlot):
     print(f'Total: {numFamilies*waveformsPerFamily}. Creating {numFamilies} waveform families with {waveformsPerFamily} waveforms per family.')
     generator = UniformWaveform.generator()
     for f in range(numFamilies):
-        x, waveforms, errorIndexes = generator.gen_uniform_based_waveform_family(waveformsPerFamily, f)
-        mplf.save_plots(x, waveforms, genFamiliesOnSinglePlot, errorIndexes, IMAGE_DIRECTORY)
+        x, waveforms, errorIndexes = generator.gen_uniform_based_waveform_family(
+            waveformsPerFamily, f)
+        mplf.save_plots(x, waveforms, genFamiliesOnSinglePlot,
+                        errorIndexes, IMAGE_DIRECTORY)
+
 
 def main():
     gen_uniform_based_waveforms(10, 5, genFamiliesOnSinglePlot=False)
+
 
 if __name__ == "__main__":
     main()
