7fbe211944ef945206d3bba1fbe4cdbc5a6e1661
Adam Arnesen <adamarnesen@gmail.com>
2021-11-01 14:06:40 -0500

    pull matplotlib printer into its own class

--- wavegen/mplformat.py
+++ wavegen/mplformat.py
@@ -7,47 +7,47 @@ import numpy.random as rnd
 import Labels
 import semivariables as sv
 
-
-def save_plot(xTitle, yTitle, classification, directory):
-    title = f'{sv.get_random_discipline()} {yTitle}'
-    current_time = datetime.now().strftime("%y%m%d%H%M%S%f")
-    filename = f'{directory}/{title}{str(current_time)}.png'
-    plt.title(title)
-    plt.xlabel(xTitle)
-    plt.ylabel(yTitle)
-    plt.savefig(filename)
-    plt.cla()
-    Labels.save_labels_to_csv(directory, [filename, classification])
-    return filename
-
-
-def save_plots(x, waveforms, genFamiliesOnSinglePlot, errorIndexes, directory):
-    os.makedirs(directory, exist_ok=True)
-    xTitle = sv.get_random_xaxis()
-    yTitle = sv.get_random_yaxis()
-    plt.figure(figsize=(7, 5), dpi=200)
-    if genFamiliesOnSinglePlot:
-        for y in waveforms:
-            plt.plot(x, y, color=random_color())
-        save_plot(xTitle, yTitle)
-    else:
-        for n in range(len(waveforms)):
-            ylim = get_max_y_axis_plot_scale(x, waveforms)
-            plt.plot(x, waveforms[n], color=random_color())
-            plt.gca().set_ylim(ylim)
-            label = 'good'
-            if n in errorIndexes:
-                label = 'bad'
-            save_plot(xTitle, yTitle, label, directory)
+class Printer:
+    def save_plot(self, xTitle, yTitle, classification, directory):
+        title = f'{sv.get_random_discipline()} {yTitle}'
+        current_time = datetime.now().strftime("%y%m%d%H%M%S%f")
+        filename = f'{directory}/{title}{str(current_time)}.png'
+        plt.title(title)
+        plt.xlabel(xTitle)
+        plt.ylabel(yTitle)
+        plt.savefig(filename)
+        plt.cla()
+        Labels.save_labels_to_csv(directory, [filename, classification])
+        return filename
 
 
-def get_max_y_axis_plot_scale(x, waveforms):
-    for w in waveforms:
-        plt.plot(x, w)
-    limits = plt.gca().get_ylim()
-    plt.cla()
-    return limits
+    def save_plots(self, x, waveforms, genFamiliesOnSinglePlot, errorIndexes, directory):
+        os.makedirs(directory, exist_ok=True)
+        xTitle = sv.get_random_xaxis()
+        yTitle = sv.get_random_yaxis()
+        plt.figure(figsize=(7, 5), dpi=200)
+        if genFamiliesOnSinglePlot:
+            for y in waveforms:
+                plt.plot(x, y, color=self.random_color())
+            self.save_plot(xTitle, yTitle)
+        else:
+            for n in range(len(waveforms)):
+                ylim = self.get_max_y_axis_plot_scale(x, waveforms)
+                plt.plot(x, waveforms[n], color=self.random_color())
+                plt.gca().set_ylim(ylim)
+                label = 'good'
+                if n in errorIndexes:
+                    label = 'bad'
+                self.save_plot(xTitle, yTitle, label, directory)
 
 
-def random_color():
-    return (rnd.random(), rnd.random(), rnd.random())
+    def get_max_y_axis_plot_scale(self, x, waveforms):
+        for w in waveforms:
+            plt.plot(x, w)
+        limits = plt.gca().get_ylim()
+        plt.cla()
+        return limits
+
+
+    def random_color(self):
+        return (rnd.random(), rnd.random(), rnd.random())
--- wavegen/randwaveform.py
+++ wavegen/randwaveform.py
@@ -9,16 +9,16 @@ def gen_random_square_wave():
     return 1
 
 
-def gen_uniform_based_waveforms(numFamilies, waveformsPerFamily, genFamiliesOnSinglePlot):
+def gen_uniform_based_waveforms(numFamilies, waveformsPerFamily, genFamiliesOnSinglePlot, printer):
     print(f"Total: {numFamilies * waveformsPerFamily}. Creating {numFamilies} waveform families with {waveformsPerFamily} waveforms per family.")
     uniform_gen = uniformwaveform.Generator()
     for f in range(numFamilies):
         x, waveforms, errorIndexes = uniform_gen.gen_uniform_based_waveform_family(waveformsPerFamily, f)
-        mplf.save_plots(x, waveforms, genFamiliesOnSinglePlot, errorIndexes, IMAGE_DIRECTORY)
+        printer.save_plots(x, waveforms, genFamiliesOnSinglePlot, errorIndexes, IMAGE_DIRECTORY)
 
 
 def main():
-    gen_uniform_based_waveforms(10, 5, genFamiliesOnSinglePlot=False)
+    gen_uniform_based_waveforms(1, 5, genFamiliesOnSinglePlot=False, printer=mplf.Printer())
 
 
 if __name__ == "__main__":
