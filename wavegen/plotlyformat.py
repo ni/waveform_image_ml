a44970f69846c37fb8cea3d3864b545ac58d7cc3
Adam Arnesen <adamarnesen@gmail.com>
2021-11-01 17:31:01 -0500

    use mode and extract series into variable

--- wavegen/plotlyformat.py
+++ wavegen/plotlyformat.py
@@ -1,11 +1,11 @@
 import os
 from datetime import datetime
 
-import numpy as np
-import plotly.express as px
+import numpy.random as rnd
 import plotly.graph_objects as go
 
 import semivariables as sv
+
 
 class Printer:
     def save_plot_family(self, x, waveformFamily, singlePlot, errorIndexes, directory):
@@ -15,14 +15,20 @@ class Printer:
         title = f'{sv.get_random_discipline()} {yTitle}'
         for waveform in waveformFamily:
             fig = go.Figure()
-            fig.add_trace(go.Scatter(x=x, y=waveform, line_color='rgb(0,100,80)'))
-            # fig.show()
+            series = go.Scatter(
+                x=x, 
+                y=waveform, 
+                line_color=self.random_color(), 
+                mode='lines')
+            fig.add_trace(series)
             fig.update_layout(
                 title=title,
                 xaxis_title=xTitle,
                 yaxis_title = yTitle,
-                plot_bgcolor="white"
             )
             current_time = datetime.now().strftime("%y%m%d%H%M%S%f")
             filename = f'{directory}/{title}{str(current_time)}.png'
             fig.write_image(filename)
+
+    def random_color(self):
+        return f'rgb({rnd.random()},{rnd.random()},{rnd.random()})'
