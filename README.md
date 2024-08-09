b92383fbfc0018ab041359f44708339cb3af614d
Adam Arnesen <adamarnesen@gmail.com>
2024-08-09 15:53:14 -0500

    move from requirements to pipfile (#9)

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Waveform Images: Machine Learning
-This repo is for the colaboration betwen [NI's Semiconductor and Electronic's](https://www.ni.com/en-us/solutions/semiconductor.html) Systems R&D team and the [BYU Colege of Physical and Mathematical Sciences](https://science.byu.edu/) Capstone projects. 
 
-# Project Overview
+This repo is for the colaboration betwen [NI's Semiconductor and Electronic's](https://www.ni.com/en-us/solutions/semiconductor.html) Systems R&D team and the [BYU Colege of Physical and Mathematical Sciences](https://science.byu.edu/) Capstone projects.
 
-## Introduction
+## Project Overview
 
-NI’s Semiconductor and Electronics Business Unit is working on a suite of products that enables Electrical Engineers working on semiconductor products to easily find and visualize their data across design, validation, and production test. 
+### Introduction
+
+NI’s Semiconductor and Electronics Business Unit is working on a suite of products that enables Electrical Engineers working on semiconductor products to easily find and visualize their data across design, validation, and production test.
 
 ![Design to Test Summary](docs/images/D2T_Overview.png)
 
@@ -13,18 +14,26 @@ Here is a video of one of my team members discussing the vision for the space we
 
 An important part of this workflow is that design and validation engineers need to be able to find, visualize, and analyze waveforms.
 
-### Waveform
+#### Waveform
+
 > A set of data series with a single independent and multiple dependent variables. Typically, the independent variable (x) is time (0ns – 10ns, step 1ps) but may also be a sweep of electrical frequencies (ex. 1MHz – 100MHz, step .1MHz), or a sweep of voltage, current, or another electrical characteristic. Waveforms typically have 100,000 to 10,000,000 points per variable.
 
-Waveforms are often captured as raw numerical results; however, they are often also captured as screen captures or images of traces from an instrument or simulation program. 
+Waveforms are often captured as raw numerical results; however, they are often also captured as screen captures or images of traces from an instrument or simulation program.
 
-## Project Vision
+### Project Vision
+
 Create a AI/ML approach to classifying images of waveform data
 
 1. Group similar waveforms.
 2. Tag images of waveforms that require further analysis due to anomalies.
 
-# Getting Started
+## Getting Started
+
 After cloning the repo, do the following:
-1. run `pip install -r requirements.txt` to install required python packages
-2. run `python RandomWaveform.py` to generate a set of waveform images
+
+1. Install [Python 3.10](https://www.python.org/downloads/release/python-3100/)
+1. Install and setup `pipenv`: `pip install pipenv`
+1. Setup environment for this project
+   1. From the root directory of the project (where the `Pipfile` file is
+      located) run `pipenv install`
+   1. Activate the environment using `pipenv shell`
--- wavegen/uniformwaveform.py
+++ wavegen/uniformwaveform.py
@@ -10,8 +10,9 @@ class Generator:
     def gen_uniform_based_waveform_family(self, waveformsPerFamily, familyNumber):
         factorOfWaveformsWithError = 0.3
         numberWithErrors = math.floor(
-            waveformsPerFamily * factorOfWaveformsWithError * rnd.random())
-        errorIndexes = rnd.randint(0, waveformsPerFamily-1, numberWithErrors)
+            waveformsPerFamily * factorOfWaveformsWithError * rnd.random()
+        )
+        errorIndexes = rnd.randint(0, waveformsPerFamily - 1, numberWithErrors)
         segments = rnd.randint(4, 8)
         xBounds = rnd.rand(2)
         xMin, xMax = np.min(xBounds), np.max(xBounds)
@@ -19,10 +20,12 @@ class Generator:
         x = np.linspace(xMin, xMax, num=xPointCount)
         y = self.__gen_random_uniform_based_waveform(x, segments)
         waveforms = []
-        for n in tqdm(range(waveformsPerFamily), desc=f'Generating Waveforms for family {familyNumber}'):
+        for n in tqdm(
+            range(waveformsPerFamily),
+            desc=f"Generating Waveforms for family {familyNumber}",
+        ):
             lineBounds = rnd.random(2)
-            line = np.linspace(
-                lineBounds.min(), lineBounds.max(), num=xPointCount)
+            line = np.linspace(lineBounds.min(), lineBounds.max(), num=xPointCount)
             ys = y * line
             if n in errorIndexes:
                 ys = self.__introduce_error(ys)
@@ -31,7 +34,7 @@ class Generator:
         return x, waveforms, errorIndexes
 
     def __gen_random_uniform_based_waveform(self, x, segments):
-        y = (x - rnd.uniform(-1, 1))
+        y = x - rnd.uniform(-1, 1)
         for s in range(segments):
             y = y * (x - rnd.uniform(-1, 1))
         return y
@@ -49,7 +52,7 @@ class Generator:
     def __introduce_narrow_spike(self, y):
         y_error = y
         min, max = np.min(y), np.max(y)
-        spike_height = np.abs(max-min) * rnd.random()
+        spike_height = np.abs(max - min) * rnd.random()
         errorPoint = rnd.randint(0, len(y))
         y_error[errorPoint] = y_error[errorPoint] + spike_height
         return y_error
@@ -58,7 +61,7 @@ class Generator:
         y_error = y
         errorPointMin, errorPointMax = self.__get_error_range(y)
         delta = self.__get_random_value_in_y_range_with_factor(y)
-        y[errorPointMin:errorPointMax] = y[errorPointMin: errorPointMax] + delta
+        y[errorPointMin:errorPointMax] = y[errorPointMin:errorPointMax] + delta
         return y_error
 
     def __introduce_constant_error(self, y):
@@ -71,7 +74,9 @@ class Generator:
     def __get_error_range(self, y):
         bounds = rnd.rand(2)
         maxErrorLength = len(y) / 2
-        return math.floor(np.min(bounds) * maxErrorLength), math.ceil(np.max(bounds) * maxErrorLength)
+        return math.floor(np.min(bounds) * maxErrorLength), math.ceil(
+            np.max(bounds) * maxErrorLength
+        )
 
     def __get_random_value_in_y_range_with_factor(self, y):
         errorScale = rnd.uniform(-1, 1)
@@ -79,10 +84,9 @@ class Generator:
         return (np.max(y) - np.min(y)) * errorScale * percentRangeForError
 
     def __add_noise(self, waveforms):
-        scale = (np.max(waveforms) - np.min(waveforms)) * \
-            np.abs(rnd.normal(0, .005))
+        scale = (np.max(waveforms) - np.min(waveforms)) * np.abs(rnd.normal(0, 0.005))
         withNoise = []
         for w in waveforms:
             noise = np.random.normal(0, scale, w.shape)
-            withNoise.append(w+noise)
+            withNoise.append(w + noise)
         return withNoise
