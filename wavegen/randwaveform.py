72f720a9a2dc01a0501ffc8d7b959fc5891761fa
Adam Arnesen <adam.arnesen@ni.com>
2024-08-09 15:52:35 -0500

    move from requirements to pipfile

--- wavegen/randwaveform.py
+++ wavegen/randwaveform.py
@@ -12,20 +12,28 @@ def gen_random_square_wave():
 
 
 def gen_uniform_based_waveforms(numFamilies, waveformsPerFamily, singlePlot, printer):
-    print(f'Generating Waveforms with {type(printer)}')
-    print(f"Total: {numFamilies * waveformsPerFamily}. Creating {numFamilies} waveform families with {waveformsPerFamily} waveforms per family.")
+    print(f"Generating Waveforms with {type(printer)}")
+    print(
+        f"Total: {numFamilies * waveformsPerFamily}. Creating {numFamilies} waveform families with {waveformsPerFamily} waveforms per family."
+    )
     uniform_gen = uniformwaveform.Generator()
     for f in range(numFamilies):
-        x, waveformFamily, errorIndexes = uniform_gen.gen_uniform_based_waveform_family(waveformsPerFamily, f)
-        printer.save_plot_family(x, waveformFamily, singlePlot, errorIndexes, IMAGE_DIRECTORY)
+        x, waveformFamily, errorIndexes = uniform_gen.gen_uniform_based_waveform_family(
+            waveformsPerFamily, f
+        )
+        printer.save_plot_family(
+            x, waveformFamily, singlePlot, errorIndexes, IMAGE_DIRECTORY
+        )
 
 
 def main():
-    formatters = [pqsv.Printer(), mplf.Printer(), ptly.Printer()]
+    formatters = [pqsv.Printer(), mplf.Printer()]
     numberOfFamilies = 3
     waveformsPerFamily = 3
     for formatter in formatters:
-        gen_uniform_based_waveforms(numberOfFamilies, waveformsPerFamily, singlePlot=True, printer=formatter)
+        gen_uniform_based_waveforms(
+            numberOfFamilies, waveformsPerFamily, singlePlot=True, printer=formatter
+        )
 
 
 if __name__ == "__main__":
